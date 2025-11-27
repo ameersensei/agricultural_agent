e2c3ad44f096815d89b250dbe92e24c8d30ad5c1
Ameer <kkhanlb@gmail.com>
2025-11-27 22:11:16 +0530

    updated research agent for cure

--- research_agent.py
+++ research_agent.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 from google.adk.agents import Agent, SequentialAgent, ParallelAgent
-from google.adk.tools import google_search
+from google.adk.tools import google_search, AgentTool
 from google.adk.sessions import InMemorySessionService
 from google.adk.runners import InMemoryRunner
 from google.genai import types
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file  
 # Using raw string (r prefix) to handle backslashes correctly
-load_dotenv(dotenv_path = r'p:\agricultural_agent\agricultural_agent\.env')
+load_dotenv(dotenv_path = r'P:\agricultural_agent\agricultural_agent\agricultural_agent\.env')
 
 # Get API key
 api_key = os.getenv('GOOGLE_API_KEY')
@@ -23,28 +23,30 @@ retry_config = types.HttpRetryOptions(
     http_status_codes = [429, 500, 503, 504],
 )
 
-research_agent = Agent(
+treatment_agent = Agent(
     model = 'gemini-2.5-flash-lite',
-    name = 'ResearchAgent',
-    description = 'ResearchAgent is a research assistant that finds research papers based on the given topic and provides a list of research papers with their titles, authors, and links to the papers.',
+    name = 'TreatmentAgent',
+    description = 'TreatmentAgent is a research agent that finds research papers on how to treat the disease mentioned by user for crops.',
     instruction = '''
-    you are a research assistant, ypur task is to find research papers based on the given topic and provide
-    a list of research papers with their titles, authors, and links to the papers.
-    1.find research paper based on topic given and only provide top 5 research papers.
-    2.never to ask further questions after providing the list of research papers.
-    3.always provide the list of research papers in the following format:
-       1. title of research paper
-       2. authors of research paper
-       3. link to research paper
-    4.use the given tools to find the research papers.
+    you are a treatment agent, you find treatment to the mentioned crop disease and provide a list of treatment with their titles, authors, and links to the papers.
+    first search for treatment using tools and make a list of it and then provide the list of treatments.
+    1.use only top 5 research papers to find treatment.
+    2.respond only for crops diseases.
+    3.instead of crop if user asks for other topic respond with "I don't know".
+    4.use the given tools to find the treatment.
     ''',
     tools = [google_search],
-    output_key = 'research_paper',
+    output_key = 'treatment',
 )
 
+
+
+
+
 runner = InMemoryRunner(
-    agent = research_agent,
+    agent = treatment_agent,
 )
 
 response = asyncio.run(runner.run_debug(input('Enter your topic: ')))
+
 
